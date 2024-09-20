065c87a19fb8a79d8f72d32d2a3470bd03626838
punit sinha <162338818+punitsinha23@users.noreply.github.com>
2024-09-20 06:52:00 +0000

    added database

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
@@ -12,7 +12,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '5791628bb0b13ce0c676dfde280b
 
 # Set the SQLAlchemy database URI to use SQLite
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#postgresql://flaskblogdatabase_xbjy_user:hkHxHcHePcohaTYeROhdcyCYn1FoayFW@dpg-crltt308fa8c739toheg-a.oregon-postgres.render.com/flaskblogdatabase_xbjy
 
 # Initialize the SQLAlchemy, Bcrypt, LoginManager, and Migrate instances
 db = SQLAlchemy(app)
@@ -20,7 +21,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-migrate = Migrate(app, db)
 
 # Import routes
 from flaskblog import routes
